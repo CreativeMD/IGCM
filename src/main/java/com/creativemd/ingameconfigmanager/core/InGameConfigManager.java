a802b941f9df4708f4f18346c2c99820bb20f111
N247S <fictiefverzonnen@gmail.com>
2015-05-29 18:03:01 +0200

    -

--- src/main/java/com/creativemd/ingameconfigmanager/core/InGameConfigManager.java
+++ src/main/java/com/creativemd/ingameconfigmanager/core/InGameConfigManager.java
@@ -12,6 +12,23 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+/**
+ * Copyright 2015 CreativeMD & N247S
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 @Mod(modid = InGameConfigManager.modid, version = InGameConfigManager.version, name = "InGameConfigManager")
 public class InGameConfigManager { 
 	
