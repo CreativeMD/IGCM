a3683144f7101e3b63ea78274867501202488248
CreativeMD <Creative-Multimedia-Design@gmx.de>
2015-10-03 14:53:13 +0200

    Fixed a server crash #2

--- src/main/java/com/creativemd/ingameconfigmanager/mod/general/GeneralBranch.java
+++ src/main/java/com/creativemd/ingameconfigmanager/mod/general/GeneralBranch.java
@@ -8,6 +8,8 @@ import com.creativemd.ingameconfigmanager.api.common.segment.BooleanSegment;
 import com.creativemd.ingameconfigmanager.api.common.segment.IntegerSegment;
 import com.creativemd.ingameconfigmanager.api.core.InGameConfigManager;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
@@ -18,6 +20,7 @@ public class GeneralBranch extends ConfigBranch{
 	}
 
 	@Override
+	@SideOnly(Side.CLIENT)
 	protected Avatar getAvatar() {
 		return new AvatarItemStack(new ItemStack(Items.paper));
 	}
