21f749eee69d8bf01c43514086f38a88f74aee79
CreativeMD <Creative-Multimedia-Design@gmx.de>
2015-11-14 13:09:04 +0100

    Added FloatSegment Added FloatSliderSegment

---
+++ src/main/java/com/creativemd/ingameconfigmanager/api/common/segment/FloatSegment.java
@@ -0,0 +1,72 @@
+package com.creativemd.ingameconfigmanager.api.common.segment;
+
+import java.util.ArrayList;
+
+import com.creativemd.creativecore.common.container.SubContainer;
+import com.creativemd.creativecore.common.container.slot.ContainerControl;
+import com.creativemd.creativecore.common.gui.SubGui;
+import com.creativemd.creativecore.common.gui.controls.GuiControl;
+import com.creativemd.creativecore.common.gui.controls.GuiTextfield;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class FloatSegment extends TitleSegment<Float>{
+	
+	public float min;
+	public float max;
+	
+	public FloatSegment(String id, String title, Float defaultValue) {
+		this(id, title, defaultValue, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
+	}
+	
+	public FloatSegment(String id, String title, Float defaultValue, float min, float max) {
+		super(id, title, defaultValue);
+		this.min = min;
+		this.max = max;
+	}
+
+	@Override
+	public ArrayList<ContainerControl> createContainerControls(
+			SubContainer gui, int x, int y, int maxWidth) {
+		ArrayList<ContainerControl> controls = new ArrayList<ContainerControl>();
+		
+		return controls;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public ArrayList<GuiControl> createGuiControls(SubGui gui, int x, int y,
+			int maxWidth) {
+		ArrayList<GuiControl> controls = super.createGuiControls(gui, x, y, maxWidth);
+		controls.add(new GuiTextfield(getID(), "" + value, x+maxWidth-50, y, 40, 20).setFloatOnly());
+		return controls;
+	}
+
+	@Override
+	public String createPacketInformation(boolean isServer) {
+		if(!isServer && guiControls != null && guiControls.size() == 2)
+		{
+			float valueBefore = value;
+			try{
+				value = Float.parseFloat(((GuiTextfield)guiControls.get(1)).text);
+			}catch(Exception e){
+				value = valueBefore;
+			}
+			value = Math.max(min, value);
+			value = Math.min(max, value);
+		}
+		return value.toString();
+	}
+
+	@Override
+	public void receivePacketInformation(String input) {
+		value = Float.parseFloat(input);
+	}
+
+	@Override
+	public boolean contains(String search) {
+		return super.contains(search) || value.toString().contains(search);
+	}
+	
+}
---
+++ src/main/java/com/creativemd/ingameconfigmanager/api/common/segment/FloatSliderSegment.java
@@ -0,0 +1,46 @@
+package com.creativemd.ingameconfigmanager.api.common.segment;
+
+import java.util.ArrayList;
+
+import com.creativemd.creativecore.common.gui.SubGui;
+import com.creativemd.creativecore.common.gui.controls.GuiAnalogeSlider;
+import com.creativemd.creativecore.common.gui.controls.GuiControl;
+import com.creativemd.creativecore.common.gui.controls.GuiTextfield;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class FloatSliderSegment extends FloatSegment{
+	
+	public FloatSliderSegment(String id, String title, Float defaultValue, float min, float max) {
+		super(id, title, defaultValue, min, max);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public ArrayList<GuiControl> createGuiControls(SubGui gui, int x, int y,
+			int maxWidth) {
+		ArrayList<GuiControl> controls = super.createGuiControls(gui, x, y, maxWidth);
+		controls.remove(1);
+		controls.add(new GuiAnalogeSlider(getID(), x+maxWidth-80, y, 70, 20, 0, 0, value, min, max));
+		return controls;
+	}
+	
+	@Override
+	public String createPacketInformation(boolean isServer) {
+		if(!isServer && guiControls != null && guiControls.size() == 2)
+		{
+			float valueBefore = value;
+			try{
+				value = ((GuiAnalogeSlider)guiControls.get(1)).value;
+			}catch(Exception e){
+				value = valueBefore;
+			}
+			value = Math.max(min, value);
+			value = Math.min(max, value);
+		}
+		return value.toString();
+	}
+	
+
+}
