7648877d58783823f5d56ac3b44b61ba4fafe6f6
CreativeMD <Creative-Multimedia-Design@gmx.de>
2016-08-17 22:11:45 +0200

    Added key default='v' to open config

---
+++ src/main/java/com/creativemd/ingameconfigmanager/api/client/IGCMClient.java
@@ -0,0 +1,30 @@
+package com.creativemd.ingameconfigmanager.api.client;
+
+import org.lwjgl.input.Keyboard;
+
+import com.creativemd.creativecore.core.CreativeCoreClient;
+import com.creativemd.ingameconfigmanager.api.core.IGCM;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class IGCMClient {
+	
+	public static KeyBinding openConfig;
+
+	
+	public static void initClient()
+	{
+		CreativeCoreClient.registerBlockItem(IGCM.advancedWorkbench);
+		
+		openConfig = new KeyBinding("key.openConfig", Keyboard.KEY_V, "key.categories.IGCM");
+		ClientRegistry.registerKeyBinding(openConfig);
+		
+		MinecraftForge.EVENT_BUS.register(new IGCMClientTick());
+	}
+	
+}
---
+++ src/main/java/com/creativemd/ingameconfigmanager/api/client/IGCMClientTick.java
@@ -0,0 +1,28 @@
+package com.creativemd.ingameconfigmanager.api.client;
+
+import com.creativemd.creativecore.common.packet.PacketHandler;
+import com.creativemd.creativecore.common.packet.gui.OpenGuiPacket;
+import com.creativemd.creativecore.gui.container.SubContainer;
+import com.creativemd.creativecore.gui.mc.ContainerSub;
+import com.creativemd.ingameconfigmanager.api.core.IGCM;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class IGCMClientTick {
+	
+	public Minecraft mc = Minecraft.getMinecraft();
+	
+	@SubscribeEvent
+	public void openGui(RenderTickEvent event)
+	{
+		if(mc.thePlayer != null && mc.theWorld != null)
+		{
+			if(mc.gameSettings.isKeyDown(IGCMClient.openConfig) && !(mc.thePlayer.openContainer instanceof ContainerSub))
+				IGCM.openModsGui(mc.thePlayer);
+		}
+	}
+	
+}
