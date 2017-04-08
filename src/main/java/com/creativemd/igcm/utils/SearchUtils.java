33aa978b64ac504f5625f8a2c410a64cbfcac182
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-04-09 00:19:40 +0200

    Fixed a small crash

--- src/main/java/com/creativemd/igcm/utils/SearchUtils.java
+++ src/main/java/com/creativemd/igcm/utils/SearchUtils.java
@@ -11,14 +11,19 @@ public class SearchUtils {
 	
 	public static boolean canStackBeFound(ItemStack stack, String search)
 	{
-		if(stack.getItem() instanceof ItemBlock)
-			if(Block.REGISTRY.getNameForObject(Block.getBlockFromItem(stack.getItem())).toString().toLowerCase().contains(search))
-				return true;
-		else
-			if(Item.REGISTRY.getNameForObject(stack.getItem()).toString().toLowerCase().contains(search))
-				return true;
-		
-		return stack.getDisplayName().toLowerCase().contains(search);
+		try
+		{
+			if(stack.getItem() instanceof ItemBlock)
+				if(Block.REGISTRY.getNameForObject(Block.getBlockFromItem(stack.getItem())).toString().toLowerCase().contains(search))
+					return true;
+			else
+				if(Item.REGISTRY.getNameForObject(stack.getItem()).toString().toLowerCase().contains(search))
+					return true;
+			
+			return stack.getDisplayName().toLowerCase().contains(search);
+		}catch(Exception e){
+			return false;
+		}
 	}
 	
 	public static boolean canInfoBeFound(InfoStack info, String search)
